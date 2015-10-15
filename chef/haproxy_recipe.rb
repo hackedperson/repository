99e30261ae164e5fc23138295e51953c7cb47a86
amitkgi <amitkgit@gmail.com>
2015-10-15 22:59:38 +0530

    chef

---
+++ chef/haproxy_recipe.rb
@@ -0,0 +1,32 @@
+package "haproxy" do
+action :install
+end
+
+ cookbook_file "/tmp/parms_to_append.conf" do
+   source "parms_to_append.conf"
+ end
+
+ bash "append_to_config" do
+   user "root"
+   code <<-EOF
+      cat /tmp/parms_to_append.conf >> /etc/haproxy/haproxy.cfg
+      rm /tmp/parms_to_append.conf
+   EOF
+   not_if "grep -q MY_IDENTIFIER /etc/config"
+ end
+
+
+
+ruby_block "provide dovecot with custom MySQL connection info" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/default/haproxy")
+    file.search_file_replace_line( "ENABLED=0" ,"ENABLED=1")
+    file.write_file
+  end
+end
+
+
+
+service 'haproxy' do
+  action [:enable, :restart]
+end
